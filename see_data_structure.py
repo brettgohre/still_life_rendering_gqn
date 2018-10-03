75544f389ccd3e86bdc07ac9db4380c55deb40ab
Brett Göhre <brettinsight@gmail.com>
2018-10-02 19:45:35 -0700

    Look at dataset structure

---
+++ see_data_structure.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+import data_reader
+
+root_path = '.'
+data_reader = DataReader(dataset='rooms_ring_camera', context_size=5, root=root_path)
+data = data_reader.read(batch_size=12)
+
+with tf.train.SingularMonitoredSession() as sess:
+    d = sess.run(data)
+    print(np.shape(d))
